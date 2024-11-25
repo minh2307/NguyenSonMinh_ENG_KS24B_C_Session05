bea37a54013e099bbd2ec9081b0181c33ea3d688
minh2307 <minh23923@gmail.com>
2024-11-25 23:57:27 +0700

    session5_bai2

---
+++ session5_bai2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	    printf("yeu cau nhap vao so 7\n");
+    while(n != 7){
+    	printf("moi nhap vao man hinh: ");
+    	scanf("%d",&n);
+	}
+	
+return 0;
+}
