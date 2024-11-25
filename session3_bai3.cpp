fe1f9b65940e3193f91dbb9d7435dc27d3d4a05a
minh2307 <minh23923@gmail.com>
2024-11-25 23:57:51 +0700

    session5_bai3

---
+++ session3_bai3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int Ban_kinh;
+	    printf("moi ban nhap vao ban kinh cua hinh tron:");
+	    scanf("%d",&Ban_kinh);
+	float pi = 3.14, chu_vi = 2*pi*Ban_kinh, dien_tich = pi*Ban_kinh*Ban_kinh; 
+	      printf("chu vi cua hinh tron là: %.2f\n",chu_vi);
+	      printf("dien tich cua hinh tron là: %.2f",dien_tich);
+}
