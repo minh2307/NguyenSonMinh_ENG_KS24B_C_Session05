f5d27d934d7b71623e0a00466351c99d0cbd7228
minh2307 <minh23923@gmail.com>
2024-11-26 11:28:26 +0700

    Update session5_bai7.cpp

--- session5_bai7.cpp
+++ session5_bai7.cpp
@@ -3,7 +3,7 @@
 int main(){
 	int a,b;
 	
-		printf("nhap a,b >0: ");
+	    printf("Nhap vao hai so nguyen duong > 0: ");
 	    scanf("%d %d",&a,&b);
 	while(b != 0){ //kiem tra khi gia tri a khac khong
 		int x = b;
